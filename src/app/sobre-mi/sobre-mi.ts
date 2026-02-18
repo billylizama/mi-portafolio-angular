214b13f02598af1b6f08e3e33fd5dd1a28e31bd0
balh <abnerlizama10@gmail.com>
2026-02-17 21:21:30 -0500

    feat: implementada interpolación de datos en sobre-mi

--- src/app/sobre-mi/sobre-mi.ts
+++ src/app/sobre-mi/sobre-mi.ts
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './sobre-mi.css',
 })
 export class SobreMiComponent {
-
+ nombre: string = 'Billy Lizama';
+  profesion: string = 'Desarrollador Frontend';
+  descripcion: string = 'Especializándome en Angular 19 para crear soluciones web de alto impacto.';
 }
