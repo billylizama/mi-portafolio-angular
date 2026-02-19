1448fed05909017ff787e7445923fd9a88445afa
balh <abnerlizama10@gmail.com>
2026-02-18 21:00:01 -0500

    feat/interactividad-contacto

--- src/app/sobre-mi/sobre-mi.ts
+++ src/app/sobre-mi/sobre-mi.ts
@@ -11,4 +11,11 @@ export class SobreMiComponent {
   profesion: string = 'Desarrollador Frontend';
   descripcion: string = 'Especializándome en Angular 19 para crear soluciones web de alto impacto.';
   habilidades: string[] = ['Angular', 'TypeScript', 'JavaScript', 'Git & GitHub', 'SQL', 'Node.js'];
+  // El "Estado": ¿se muestra el contacto o no?
+mostrarContacto: boolean = false;
+
+// La "Acción": una función para cambiar el estado
+alternarContacto() {
+  this.mostrarContacto = !this.mostrarContacto;
+}
 }
