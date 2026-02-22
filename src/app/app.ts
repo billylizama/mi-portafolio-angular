9dcc63723ec535ba7fa0828c06d7f7c8b4e3addc
balh <abnerlizama10@gmail.com>
2026-02-22 15:59:42 -0500

    feat: arquitectura de servicios completada y proyectos dinámicos

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,18 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SobreMiComponent } from './sobre-mi/sobre-mi';
 import { ProyectoCardComponent } from './proyecto-card/proyecto-card';
+import { ProyectosService } from './services/proyectos'; 
 @Component({
   selector: 'app-root',
-  imports: [SobreMiComponent,ProyectoCardComponent],
+  imports: [RouterOutlet, SobreMiComponent,ProyectoCardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  misProyectos: any[] = [];
+  constructor(private proyectosService: ProyectosService) {
+    this.misProyectos = this.proyectosService.obtenerProyectos();
+  }
   protected readonly title = signal('mi-portafolio-angular');
+  
 }
